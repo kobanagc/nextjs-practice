a8c3a631df92caa15eee4585c56debde81295eb8
kobanagc <naoki.kobayashi.0708@gmail.com>
2023-05-02 00:01:00 +0900

    ♻️ Componentize Main part

--- src/components/Path.tsx
+++ src/components/Path.tsx
@@ -1,15 +1,14 @@
-import styles from '@/styles/Home.module.css'
-
 // Propsの型を定義
 interface Props {
-  page: string;
+  children: React.ReactNode;
 }
+
 
 export function Path(props: Props) {
   return (
     <p>
       Get started by editing&nbsp;
-      <code className={styles.code}>pages/{props.page}.tsx</code>
+      {props.children}
     </p>
   )
 }
