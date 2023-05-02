0a1f2babdcd682790ac2057c62444ee5c8ede160
kobanagc <naoki.kobayashi.0708@gmail.com>
2023-05-02 15:38:23 +0900

    💄 Split CSS modules

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from './Main.module.css'
 import { Logo } from '@/components/Logo'
 import { Links } from '@/components/Links'
 import { MainImage } from '@/components/MainImage'
