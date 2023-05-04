b3cae98854a6b2245e4faf566d7b9a09318eb4b7
kobanagc <naoki.kobayashi.0708@gmail.com>
2023-05-04 18:05:47 +0900

    ⚗️ Change background color by using useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head'
 import { Main } from '@/components/Main'
 import { Header } from '@/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 const foo = "hoge"
 
 export default function Home() {
-const handleClick = useCallback(()=> { {/* useCallbackを使うとfunctionが読み込まれてもhandleClickは読み込まれなくなる*/}
+  useEffect(() => {
+    // DOMを直接操作するのはReactでは原則NGだが今回は背景色を変える程度なので下記で実装。
+    document.body.style.backgroundColor = "navy" // マウント時
+
+    return () => {
+      document.body.style.backgroundColor = "" //アンマウント時
+    }
+  }, [])
+
+  const handleClick = useCallback(()=> { // useCallbackを使うとfunctionが読み込まれてもhandleClickは読み込まれなくなる
     alert("クリック！！")
   },[])
+
   return (
     <>
       <Head>
